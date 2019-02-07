3d75b1e35e2aed064271f182b8997dcff729e45d
Benjamin Goldberg <ben@spothero.com>
2019-02-07 06:38:27 -0800

    Differentiate demo3, lesson2 from exercise

--- demos/dags/lesson2/demo3.py
+++ demos/dags/lesson2/demo3.py
@@ -15,26 +15,7 @@ def load_trip_data_to_redshift(*args, **kwargs):
     credentials = aws_hook.get_credentials()
     redshift_hook = PostgresHook("redshift")
 
-    # # #
-    # TODO: How do we get the execution_date from our context?
-    # execution_date=kwargs["<REPLACE>"]
-    execution_date = datetime.datetime.utcnow()
-    # # #
-
-    sql_stmt = sql.COPY_MONTHLY_TRIPS_SQL.format(
-        credentials.access_key,
-        credentials.secret_key,
-        year=execution_date.year,
-        month=execution_date.month
-    )
-    redshift_hook.run(sql_stmt)
-
-
-def load_station_data_to_redshift(*args, **kwargs):
-    aws_hook = AwsHook("aws_credentials")
-    credentials = aws_hook.get_credentials()
-    redshift_hook = PostgresHook("redshift")
-    sql_stmt = sql.COPY_STATIONS_SQL.format(
+    sql_stmt = sql.COPY_ALL_TRIPS_SQL.format(
         credentials.access_key,
         credentials.secret_key,
     )
@@ -60,21 +41,17 @@ copy_trips_task = PythonOperator(
     task_id='load_trips_from_s3_to_redshift',
     dag=dag,
     python_callable=load_trip_data_to_redshift,
-    # TODO: Provide context
 )
 
-create_stations_table = PostgresOperator(
-    task_id="create_stations_table",
+location_traffic_task = PostgresOperator(
+    task_id="calculate_location_traffic",
     dag=dag,
     postgres_conn_id="redshift",
-    sql=sql.CREATE_STATIONS_TABLE_SQL,
-)
-
-copy_stations_task = PythonOperator(
-    task_id='load_stations_from_s3_to_redshift',
-    dag=dag,
-    python_callable=load_station_data_to_redshift,
+    # TODO: Use prev_ds and next_ds to scope the location traffic task
+    sql=f"""
+        {sql.LOCATION_TRAFFIC_SQL}
+    """
 )
 
 create_trips_table >> copy_trips_task
-create_stations_table >> copy_stations_task
+copy_trips_task >> location_traffic_task
